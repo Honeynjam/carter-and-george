42f499a6185c299aee15123261729055015de70c
kb1995 <kristiyan.r.bogdanov@gmail.com>
2024-10-06 13:05:02 +0100

    Bullet Section

--- components/page-sections/BulletsSection.js
+++ components/page-sections/BulletsSection.js
@@ -1,11 +1,58 @@
+import { Check } from "@phosphor-icons/react";
 import { storyblokEditable } from "@storyblok/react";
+import cn from "classnames";
 
+import { linkResolver } from "utils/linkResolver";
+
+import Button from "components/common/Button";
 import Container from "components/common/Container";
+import { Eyebrow, Heading, Subtitle } from "components/common/Typography";
 
 const BulletsSection = ({ blok }) => {
   return (
-    <div {...storyblokEditable(blok)}>
-      <Container>Bullets Section - TODO</Container>
+    <div className="section-spacing-p bg-stone" {...storyblokEditable(blok)}>
+      <Container>
+        <div className="grid gap-8 lg:grid-cols-12 lg:gap-20">
+          <div className="lg:col-span-5">
+            <Eyebrow text={blok.eyebrow} />
+            <Heading size="3xl" level={2} className="mb-4">
+              {blok.title}
+            </Heading>
+            <Subtitle color="grey">{blok.subtitle}</Subtitle>
+            <div className="mt-6 lg:mt-12">
+              {blok.buttons.map((button) => {
+                return <Button href={linkResolver(button.link)}>{button.text}</Button>;
+              })}
+            </div>
+          </div>
+          <div className="lg:col-span-7">
+            <div
+              className={cn({
+                "grid grid-cols-2 gap-x-12 gap-y-4": blok.bullet_columns === "two",
+              })}
+            >
+              {blok.bullet_list.map((item) => {
+                if (item.text) {
+                  return (
+                    <div
+                      key={item._uid}
+                      className={cn("flex items-center gap-4 text-md text-black", {
+                        "border-t border-stroke-light py-6 lg:py-8": blok.bullet_columns === "one",
+                        "": blok.bullet_columns === "two",
+                      })}
+                    >
+                      <div className="rounded-full bg-[#E9F0F2] p-1.5">
+                        <Check className="h-6 w-6 text-blue" weight="bold" />
+                      </div>
+                      <span className="flex-1">{item.text}</span>
+                    </div>
+                  );
+                } else return null;
+              })}
+            </div>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
