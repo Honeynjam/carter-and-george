c48de5570c722e097804f037e4c1e2f1c4f1501c
kb1995 <kristiyan.r.bogdanov@gmail.com>
2024-10-07 14:22:43 +0100

    Add sitemap/robots

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,55 @@
+import { renderToStaticMarkup } from "react-dom/server";
+
+import { nonPageFolders } from "config/storyblok";
+
+import { queryAllPages } from "utils/queryAllPages";
+
+const SitemapIndex = () => null;
+
+const includePage = (page) => {
+  if (["register_page", "sign_up", "video"].includes(page.content.component)) {
+    return false;
+  }
+
+  return !(page.content.seo_index === "false");
+};
+
+const Sitemap = ({ pages }) => {
+  const origin = "https://www.carterandgeorge.co.uk/";
+
+  return (
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      {pages.map((page, index) => {
+        const lastModified = new Date(page.published_at).toISOString();
+
+        if (page.full_slug) {
+          const path = page.full_slug === "home" ? "" : page.full_slug?.replace(/\/?$/, "/");
+          const url = origin + path;
+          if (includePage(page)) {
+            return (
+              <url key={index}>
+                <loc>{url}</loc>
+                <lastmod>{lastModified}</lastmod>
+              </url>
+            );
+          }
+        }
+      })}
+    </urlset>
+  );
+};
+
+export const getServerSideProps = async ({ res }) => {
+  const data = await queryAllPages();
+  const pages = data.filter((story) => !nonPageFolders.some((v) => story.full_slug.includes(v)));
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(renderToStaticMarkup(<Sitemap pages={pages} />));
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default SitemapIndex;
---
+++ utils/queryAllPages.js
@@ -0,0 +1,24 @@
+import { getStoryblokApi } from "@storyblok/react";
+
+export const queryAllPages = async (page = 1, routes = []) => {
+  const per_page = 100;
+  const storyblokApi = getStoryblokApi();
+  let sbParams = {
+    // version: "published",
+    version: "draft",
+    per_page: per_page,
+    page: page,
+  };
+
+  let { data, headers } = await storyblokApi.get(`cdn/stories`, sbParams);
+  const allRoutes = routes.concat(data.stories);
+
+  const total = headers.total;
+  const totalPages = Math.ceil(total / per_page);
+
+  if (totalPages > page) {
+    return queryAllPages(page + 1, allRoutes);
+  } else {
+    return allRoutes;
+  }
+};
