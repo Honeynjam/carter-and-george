68f61c5e1bb616f9a4ebc5bf5901518cf9fed8d3
kb1995 <kristiyan.r.bogdanov@gmail.com>
2024-10-11 12:35:23 +0100

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" type="image/webp" href="/favicon.webp" />
         <link rel="stylesheet" href="https://use.typekit.net/wyr7rga.css" />
       </Head>
       <body className="antialiased">
