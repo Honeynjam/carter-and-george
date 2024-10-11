e870a7802cadba9a8dfa56b9a2ad97aa1e59fd19
kb1995 <kristiyan.r.bogdanov@gmail.com>
2024-10-11 20:19:11 +0100

    Updates

--- components/page-content-types/LeaderProfile.js
+++ components/page-content-types/LeaderProfile.js
@@ -1,6 +1,7 @@
 import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
 import { render } from "storyblok-rich-text-react-renderer";
 
+import { arrayNotEmpty } from "utils/arrayNotEmpty";
 import { linkResolver } from "utils/linkResolver";
 import { truncate } from "utils/truncate";
 
@@ -11,18 +12,18 @@ import { Heading, Subtitle } from "components/common/Typography";
 import StoryblokImage from "components/storyblok/StoryblokImage";
 
 const LeaderProfile = ({ blok }) => (
-  <div className="mt-20" {...storyblokEditable(blok)} key={blok._uid}>
+  <div className="mt-12 lg:mt-20" {...storyblokEditable(blok)} key={blok._uid}>
     <Container>
-      <div className="mb-2xl">
+      <div className="mb-xl lg:mb-2xl">
         <Breadcrumbs
           data={[
             { name: "About", href: "/about/" },
             { name: blok.name, current: true },
           ]}
         />
-        <hr className="mb-12 mt-6 text-stroke-light" />
+        <hr className="mb-12 mt-6 text-stroke-light max-lg:hidden" />
         <div className="grid items-start gap-12 lg:grid-cols-12">
-          <div className="border-stroke-light lg:col-span-9 lg:border-r lg:pr-12">
+          <div className="lg:col-span-9">
             <Heading className="mb-8" size="3xl" level={1}>
               {blok.name}
             </Heading>
@@ -32,7 +33,7 @@ const LeaderProfile = ({ blok }) => (
               image={blok.image}
             />
 
-            <div className="prose mt-14 max-w-none lg:prose-lg">
+            <div className="prose mt-8 max-w-none lg:prose-lg lg:mt-14">
               {render(blok.text, {
                 defaultBlokResolver: (name, props) => {
                   const blok = { ...props, component: name };
@@ -41,31 +42,36 @@ const LeaderProfile = ({ blok }) => (
               })}
             </div>
           </div>
-          <div className="lg:sticky lg:top-32 lg:col-span-3">
-            <Heading className="mb-12" size="large">
-              {blok.recommended_services_title}
-            </Heading>
-            <div className="grid grid-cols-1 gap-12">
-              {blok.recommended_services &&
-                blok.recommended_services?.map((item) => {
-                  if (item.content) {
-                    return (
-                      <div key={item._uid} className="border-b border-stroke-light pb-8">
-                        <Heading className="mb-2" level={3} size="medium">
-                          {item.content.card_title}
-                        </Heading>
-                        <Subtitle>{truncate(item.content.card_subtitle, 100)}</Subtitle>
-                        <TextButton className="mt-12 w-1/2 justify-start" href={linkResolver(item)}>
-                          Find out more
-                        </TextButton>
-                      </div>
-                    );
-                  }
+          {arrayNotEmpty(blok.recommended_services) ? (
+            <div className="border-stroke-light lg:sticky lg:top-32 lg:col-span-3 lg:border-l lg:pl-12">
+              <Heading className="mb-12" size="large">
+                {blok.recommended_services_title}
+              </Heading>
+              <div className="grid grid-cols-1 gap-12">
+                {blok.recommended_services &&
+                  blok.recommended_services?.map((item) => {
+                    if (item.content) {
+                      return (
+                        <div key={item._uid} className="border-b border-stroke-light pb-8">
+                          <Heading className="mb-2" level={3} size="medium">
+                            {item.content.card_title}
+                          </Heading>
+                          <Subtitle>{truncate(item.content.card_subtitle, 100)}</Subtitle>
+                          <TextButton
+                            className="mt-12 w-1/2 justify-start"
+                            href={linkResolver(item)}
+                          >
+                            Find out more
+                          </TextButton>
+                        </div>
+                      );
+                    }
 
-                  return null;
-                })}
+                    return null;
+                  })}
+              </div>
             </div>
-          </div>
+          ) : null}
         </div>
       </div>
     </Container>
