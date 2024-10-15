c46251f1e513f2fb7440e1ece646016e25163e00
kb1995 <kristiyan.r.bogdanov@gmail.com>
2024-10-15 16:10:34 +0100

    Storyblok redirects

--- next.config.js
+++ next.config.js
@@ -1,54 +1,52 @@
-const { createSecureHeaders } = require("next-secure-headers");
+const StoryblokClient = require("storyblok-js-client");
 const redirects = require("./config/redirects");
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const config = {
-  trailingSlash: true,
+module.exports = async () => {
+  // Put your plugins and its options here
+  // Inspired by https://github.com/hashicorp/next-mdx-enhanced/issues/18#issuecomment-859167393
+  const plugins = [withBundleAnalyzer];
 
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    // deviceSizes: [375, 768, 1024, 1280, 1440, 1680, 2500, 3840],
-    domains: ["a.storyblok.com", "s3.amazonaws.com"],
-  },
-  // async headers() {
-  //   const secureHeaders = createSecureHeaders({
-  //     forceHTTPSRedirect: [true, { includeSubDomains: true, preload: true }],
-  //     frameGuard: false,
-  //     xssProtection: "block-rendering",
-  //     referrerPolicy: "strict-origin-when-cross-origin",
-  //     nosniff: "nosniff",
-  //   }).concat([
-  //     {
-  //       key: "Permissions-Policy",
-  //       value: "camera=(), microphone=(), geolocation=()",
-  //     },
-  //   ]);
+  // Storyblok redirects
+  // ----
+  let Storyblok = new StoryblokClient({
+    accessToken: process.env.NEXT_PUBLIC_STORYBLOK_TOKEN,
+    cache: {
+      clear: "auto",
+      type: "memory",
+    },
+  });
 
-  //   return [
-  //     {
-  //       source: "/(.*)",
-  //       headers: secureHeaders,
-  //     },
-  //     // In order to match the root path (`/`), we need to specify `/:path*` as
-  //     // well. See also:
-  //     // - https://github.com/jagaapple/next-secure-headers/tree/v2.2.0#use-createsecureheaders-in-nextconfigjs-recommended
-  //     // - https://github.com/vercel/next.js/issues/25243.
-  //     {
-  //       source: "/:path*",
-  //       headers: secureHeaders,
-  //     },
-  //   ];
-  // },
-  async redirects() {
-    return redirects;
-  },
+  const { data } = await Storyblok.get("cdn/stories/global/redirects", {
+    version: "published",
+  });
+
+  const storyblokRedirects = data.story.content.entries.map((item) => {
+    return {
+      source: item.source_url,
+      destination: item.destination_url,
+      permanent: true,
+    };
+  });
+  // ----
+
+  return plugins.reduce((acc, next) => next(acc), {
+    trailingSlash: true,
+
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      // deviceSizes: [375, 768, 1024, 1280, 1440, 1680, 2500, 3840],
+      domains: ["a.storyblok.com", "s3.amazonaws.com"],
+    },
+    async redirects() {
+      return [...redirects, ...storyblokRedirects];
+    },
+  });
 };
-
-module.exports = withBundleAnalyzer(config);
