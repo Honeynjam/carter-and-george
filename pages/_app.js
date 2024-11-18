4f2a60bd17ee2299353ccc4ef148c3108c2a5dfa
kb1995 <kristiyan.r.bogdanov@gmail.com>
2024-11-18 17:19:11 +0100

    Update key

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ import StoryblokWrapper from "components/storyblok/StoryblokWrapper";
 
 export default function App({ Component, pageProps }) {
   return (
-    <APIProvider apiKey={"AIzaSyCQm4y_CgaoI1XKXJxTpVY3B1EEEC7G4UY"}>
+    <APIProvider apiKey={"AIzaSyBELN_3if61U7k0iO6IzMe7aDVb6kXQmJI"}>
       <Script
         id="iubenda"
         dangerouslySetInnerHTML={{
